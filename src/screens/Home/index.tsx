e670d36467714df284d12b45aeef58b34aa5b47c
brad <b.bradfrancis@gmail.com>
2020-05-21 14:26:24 +1000

    Blocked out Homepage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -7,6 +7,29 @@ class Home extends React.Component {
 		return(
 			<div className='main-container'>
 				<Header />
+				<div className='banner-container'>
+
+				</div>
+				<div className='brands-container'>
+					<img src={require('../../images/Brands/Layer1.png')} />
+					<img src={require('../../images/Brands/Layer2.png')} />
+					<img src={require('../../images/Brands/Layer3.png')} />
+					<img src={require('../../images/Brands/Layer4.png')} />
+					<img src={require('../../images/Brands/Layer5.png')} />
+					<img src={require('../../images/Brands/Layer6.png')} />
+					<img src={require('../../images/Brands/Layer7.png')} />
+				</div>
+				<div className='about-container'>
+					<h2>Who We Are</h2>
+					<div className='about-pargraph-container'>
+						<p>Australian RV Accessories are quality importers, exporters and wholesalers of RV accessories to the caravan and motorhome industries of Australia and New Zealand.
+
+	Australian RV Accessories are one of the foremost RV and caravan accessory suppliers in Australia. Our aim is to provide a quality service to the wholesale RV market and give customers an alternative and equitable supply with new and innovative products.</p>
+						<p>Australian RV Accessories are distributors of Shurflo, Collapsible Space Saving Products, Royal Kitchenware, On The Road RV, Fan-Tastic Portable Cooling Solutions, Premier, RV Flomaster, Dirty Devil, Vision Plus Replacement Towing Mirrors and more.
+
+	Whilst we welcome all enquiries please be aware that we are a wholesale only business and so not sell direct to the public. Please call for the name of your nearest stockist.</p>
+					</div>
+				</div>
 			</div>
 		)
 	}
