f725462f077ca84848474e490064c1ce3e98f0d5
brad <b.bradfrancis@gmail.com>
2020-06-18 22:29:19 +1000

    Added a Basic Mobile Menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './index.css';
 import { Link } from 'react-router-dom';
+import { IoMdMenu, IoIosClose } from 'react-icons/io';
 
-class Header extends React.Component {
-	render(){
+interface IState {
+	isMenuOpen: boolean,
+}
+
+class Header extends React.Component<{}, IState> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			isMenuOpen: false
+		}
+	}
+
+	render() {
 		return(
 			<div className="nav-container">
 				<div className="header-container">
 					<div className="arva-logo">
-						<img src={require('../../images/Logo.png')}></img>
+						<img className="arva-logo" src={require('../../images/Logo.png')}></img>
 					</div>
 					<div className="right-container">
 						<div id='circleDivider' />
@@ -17,6 +29,9 @@ class Header extends React.Component {
 							<button id='contact-button'>Contact Us</button>
 						</div>
 					</div>
+					<button onClick={() => this.setState({ isMenuOpen: true })} className="nav-hamburger-container">
+						<IoMdMenu style={{width: '50px', height: '50px'}} />
+					</button>
 				</div>
 				<nav className="navbar-container">
 					<Link id="nav-link" to="/">HOME</Link>
@@ -25,6 +40,14 @@ class Header extends React.Component {
 					<Link id="nav-link" to="/brochures">BROCHURES</Link>
 					<Link id="nav-link" to="/contact">CONTACT</Link>
 				</nav>
+				<div style={{ userSelect: this.state.isMenuOpen ? "all" : 'none', opacity: this.state.isMenuOpen? 1 : 0}} className="nav-container-hamburger">
+					<button onClick={() => this.setState({ isMenuOpen: false })}><IoIosClose style={{width: '50px', height: '50px'}}/></button>
+					<Link id="nav-link" to="/">HOME</Link>
+					<Link id="nav-link" to="/stockists">STOCKISTS</Link>
+					<Link id="nav-link" to="/brands">BRANDS</Link>
+					<Link id="nav-link" to="/brochures">BROCHURES</Link>
+					<Link id="nav-link" to="/contact">CONTACT</Link>
+				</div>
 			</div>
 		)
 	}
