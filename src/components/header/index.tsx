33ba153263c7e8219b8778399892cb39da0b009e
Brad Francis <b.bradfrancis@gmail.com>
2020-06-26 14:37:21 +1000

    Added react-spring

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,57 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import { Link } from 'react-router-dom';
 import { IoMdMenu, IoIosClose } from 'react-icons/io';
 import { OpenCatalog } from '../../util';
+import { useSpring, animated} from 'react-spring';
 
 interface IState {
 	isMenuOpen: boolean,
 }
 
-class Header extends React.Component<{}, IState> {
-	constructor(props: any) {
-		super(props);
-		this.state = {
-			isMenuOpen: false
-		}
-	}
+const Header = () => {
+	const [isMenuOpen, setisMenuOpen] = useState(true);
 
-	render() {
-		return(
-			<div className="nav-container">
-				<div className="header-container">
-					<div className="arva-logo">
-						<img className="arva-logo" src={require('../../images/Logo.png')}></img>
-					</div>
-					<div className="right-container">
-						<div id='circleDivider' />
-						<div id='content-wrapper'>
-							{/* <button id='login-button'>Login</button> */}
-							<button id='contact-button' onClick={OpenCatalog}>View Catalog</button>
-						</div>
-					</div>
-					<button onClick={() => this.setState({ isMenuOpen: true })} className="nav-hamburger-container">
-						<IoMdMenu style={{width: '50px', height: '50px'}} />
-					</button>
+	const springProps = useSpring({
+		height: isMenuOpen ? '30rem' : '0',
+	})
+
+
+	return(
+		<div className="nav-container">
+			<div className="header-container">
+				<div className="arva-logo">
+					<img className="arva-logo" src={require('../../images/Logo.png')}></img>
 				</div>
-				<nav className="navbar-container">
-					<Link id="nav-link" to="/">HOME</Link>
-					<Link id="nav-link" to="/stockists">STOCKISTS</Link>
-					<Link id="nav-link" to="/brands">BRANDS</Link>
-					<Link id="nav-link" to="/brochures">BROCHURES</Link>
-					<Link id="nav-link" to="/contact">CONTACT</Link>
-				</nav>
-				{this.state.isMenuOpen && <div className="nav-container-hamburger">
-					<button onClick={() => this.setState({ isMenuOpen: false })}><IoIosClose style={{ width: '50px', height: '50px' }} /></button>
-					<Link id="nav-link" to="/">HOME</Link>
-					<Link id="nav-link" to="/stockists">STOCKISTS</Link>
-					<Link id="nav-link" to="/brands">BRANDS</Link>
-					<Link id="nav-link" to="/brochures">BROCHURES</Link>
-					<Link id="nav-link" to="/contact">CONTACT</Link>
-				</div>}
+				<div className="right-container">
+					<div id='circleDivider' />
+					<div id='content-wrapper'>	
+						{/* <button id='login-button'>Login</button> */}
+						<button id='contact-button' onClick={OpenCatalog}>View Catalog</button>
+					</div>
+				</div>
+				<button onClick={() => setisMenuOpen(true)} className="nav-hamburger-container">
+					<IoMdMenu style={{width: '50px', height: '50px'}} />
+				</button>
 			</div>
-		)
-	}
+			<nav className="navbar-container">
+				<Link id="nav-link" to="/">HOME</Link>
+				<Link id="nav-link" to="/stockists">STOCKISTS</Link>
+				<Link id="nav-link" to="/brands">BRANDS</Link>
+				<Link id="nav-link" to="/brochures">BROCHURES</Link>
+				<Link id="nav-link" to="/contact">CONTACT</Link>
+			</nav>
+			<animated.div style={springProps} className="nav-container-hamburger">
+				<button onClick={() => setisMenuOpen(false)}><IoIosClose style={{ width: '50px', height: '50px' }} /></button>
+				<Link id="nav-link" to="/">HOME</Link>
+				<Link id="nav-link" to="/stockists">STOCKISTS</Link>
+				<Link id="nav-link" to="/brands">BRANDS</Link>
+				<Link id="nav-link" to="/brochures">BROCHURES</Link>
+				<Link id="nav-link" to="/contact">CONTACT</Link>
+			</animated.div>
+		</div>
+	)
 }
+
 
 export default Header;
