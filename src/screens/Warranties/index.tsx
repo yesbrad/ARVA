1bbe998995784444846630475a5d00aa7c401c71
brad <b.bradfrancis@gmail.com>
2021-03-11 14:24:12 +1100

    Warranties

--- src/screens/Warranties/index.tsx
+++ src/screens/Warranties/index.tsx
@@ -8,22 +8,36 @@ import { apiURL } from '../../api';
 const sgMail = require('@sendgrid/mail')
 
 interface IState {
+	isCompany: boolean,
 	sent: string,
-	message: string,
-	subject: string,
+	companyName: string,
+	deliveryAddress: string,
+	phone: string,
 	email: string,
+	quantity: string,
+	productDescription: string,
+	serialNumber: string,
 	name: string,
+	invoiceNumber: string,
+	purchasedFrom: string,
 }
 
 class Warranties extends React.Component<{}, IState> {
 	constructor(props: any) {
 		super(props);
 		this.state = {
+			isCompany: false,
 			sent: '',
-			message: '',
-			subject: '',
+			companyName: '',
 			email: '',
-			name:'',
+			name: '',
+			phone: '',
+			deliveryAddress: '',
+			quantity: '',
+			productDescription: '',
+			serialNumber: '',
+			invoiceNumber: '',
+			purchasedFrom: '',
 		}
 	}
 
@@ -32,21 +46,28 @@ class Warranties extends React.Component<{}, IState> {
 
 		this.setState({ sent: 'Sending' });
 
-		const { message, email, name, subject } = this.state;
+		const { email, name, companyName,phone,deliveryAddress,productDescription,serialNumber,quantity,invoiceNumber,purchasedFrom } = this.state;
 
-		if (message === '' || email === '' || name === '' || subject === '') {
+		if (email === '' || name === '' || phone === '' || deliveryAddress === '' || productDescription === ''
+			|| serialNumber === '' || invoiceNumber === '') {
 			this.setState({ sent: '*Missing Fields' });
 			return;
 		}
 
 		try {	
-			await fetch(`${apiURL}/sendMail`, {
+			await fetch(`${apiURL}/sendWarrenty`, {
 				method: 'POST',
 				body: JSON.stringify({
-					message,
+					companyName,
 					email,
-					subject,
 					name,
+					phone,
+					deliveryAddress,
+					quantity,
+					productDescription,
+					serialNumber,
+					invoiceNumber,
+					purchasedFrom,
 				}),
 			});
 			this.setState({ sent: 'Sent' });
@@ -73,17 +94,25 @@ class Warranties extends React.Component<{}, IState> {
 							<p id="cont-head">WARRANTIES</p>
 							<p id="bold">Please note we are a wholesale only business and do not sell direct to the public.</p>
 							<p>The below emails are for trade enquiries only - please do not send orders by the below address.  It is not checked for orders and your order may not be dispatched.</p>
+							<div className="selection-container">
+								<button id={this.state.isCompany && "selected"} className="select-button-left" onClick={()=> this.setState({ isCompany: !this.state.isCompany })}>Company</button>
+								<button id={!this.state.isCompany && "selected"} className="select-button-right" onClick={()=> this.setState({ isCompany: !this.state.isCompany })}>Individual</button>
+							</div>
 						</div>
 						<div id="warranties-form">
-							<p>Name</p>
-							<textarea value={this.state.name} onChange={(event) => this.setState({ name: event.target.value})} id="name-box"></textarea>
-							<p>Email</p>
-							<textarea value={this.state.email} onChange={(event) => this.setState({ email: event.target.value})}  id="name-box"></textarea>
-							<p>Subject Line</p>
-							<textarea value={this.state.subject} onChange={(event) => this.setState({ subject: event.target.value})}  id="name-box"></textarea>
-							<p>Message</p>
-							<textarea value={this.state.message} onChange={(event) => this.setState({ message: event.target.value})}  id="large-box"></textarea>
-							<button onClick={this.onSendEmail}>Submit</button>
+							{this.state.isCompany && <><p>Company Name</p><textarea value={this.state.companyName} onChange={(event) => this.setState({ companyName: event.target.value})} id="name-box"></textarea></>}
+							<p>Contact Name</p><textarea value={this.state.name} onChange={(event) => this.setState({ name: event.target.value})} id="name-box"></textarea>
+							<p>Delivery Address</p><textarea value={this.state.deliveryAddress} onChange={(event) => this.setState({ deliveryAddress: event.target.value})} id="name-box"></textarea>
+							<p>Phone</p><textarea value={this.state.phone} onChange={(event) => this.setState({ phone: event.target.value})} id="name-box"></textarea>
+							<p>Email</p><textarea value={this.state.email} onChange={(event) => this.setState({ email: event.target.value})} id="name-box"></textarea>
+							{this.state.isCompany && <><p>Quantity</p><textarea value={this.state.quantity} onChange={(event) => this.setState({ quantity: event.target.value })} id="name-box"></textarea></>}
+							{/* <p>Product Description</p><textarea value={this.state.productDescription} onChange={(event) => this.setState({ productDescription: event.target.value})} id="name-box"></textarea> */}
+							<p>Serial No</p><textarea value={this.state.serialNumber} onChange={(event) => this.setState({ serialNumber: event.target.value})} id="name-box"></textarea>
+							{!this.state.isCompany && <><p>Purchased From</p><textarea value={this.state.purchasedFrom} onChange={(event) => this.setState({ purchasedFrom: event.target.value})} id="name-box"></textarea></>}
+							<p>Invoice Number</p><textarea value={this.state.invoiceNumber} onChange={(event) => this.setState({ invoiceNumber: event.target.value})} id="name-box"></textarea>
+							<p>Description</p>
+							<textarea value={this.state.productDescription} onChange={(event) => this.setState({ productDescription: event.target.value})}  id="large-box"></textarea>
+							<button onClick={this.onSendEmail}>Submit Warranty</button>
 							{this.state.sent && <span style={{ color: this.state.sent === 'Sent' ? 'green' : 'red' }}>{this.state.sent}</span>}
 						</div>
 					</div>
